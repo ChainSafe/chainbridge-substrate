9a3037806e8c8f93ff707e52a4a38812c66a3605
David Ansermino <david@chainsafe.io>
2020-03-05 09:35:02 -0500

    Add travis (#1)

---
+++ scripts/ci.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eux
+
+RUST_TOOLCHAIN="${RUST_TOOLCHAIN:-nightly}"
+
+# Enable warnings about unused extern crates
+export RUSTFLAGS=" -W unused-extern-crates"
+
+# Install rustup and the specified rust toolchain.
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Load cargo environment. Specifically, put cargo into PATH.
+source ~/.cargo/env
+
+sudo apt-get -y update
+sudo apt-get install -y cmake pkg-config libssl-dev
+
+./scripts/init.sh
+
+rustc --version
+rustup --version
+cargo --version
+
+case $TARGET in
+	"build")
+		cargo build --release --locked "$@"
+		;;
+
+	"runtime-test")
+		cargo test -p chainbridge
+		;;
+
+  "fmt")
+    rustup component add rustfmt
+    cargo fmt --all -- --check
+    ;;
+esac
